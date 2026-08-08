c2258c30b3ea52910f1ee68c2bd0ca3068d896c5
miku332 <112794032+miku332@users.noreply.github.com>
2026-08-09 02:39:16 +0800

    Add AngryBird

---
+++ src/Cards/AngryBird.cs
@@ -0,0 +1,55 @@
+using MegaCrit.Sts2.Core.Commands;
+using MegaCrit.Sts2.Core.Entities.Cards;
+using MegaCrit.Sts2.Core.GameActions.Multiplayer;
+using MegaCrit.Sts2.Core.Localization.DynamicVars;
+using MegaCrit.Sts2.Core.ValueProps;
+using STS2RitsuLib.Cards.DynamicVars;
+using STS2RitsuLib.Interop.AutoRegistration;
+using STS2RitsuLib.Scaffolding.Content;
+using GuZhenRen.CardPools;
+using GuZhenRen.Powers;
+using GuZhenRen.Tags;
+
+namespace GuZhenRen.Cards;
+
+[RegisterCard(typeof(GuZhenRenCardPool))]
+public sealed class AngryBird : GuZhenRenCardTemplate
+{
+    public override int Rank => 1;
+
+    public override CardAssetProfile AssetProfile => new(
+        PortraitPath: "res://GuZhenRen/images/cards/AngryBird.png");
+
+    public override IEnumerable<CardTag> Tags => [GuZhenRenTags.YanDao];
+
+    protected override IEnumerable<DynamicVar> CanonicalVars =>
+    [
+        new DamageVar(19, ValueProp.Move),
+        new PowerVar<FenShaoPower>(19)
+    ];
+
+    public AngryBird()
+        : base(2, CardType.Attack, CardRarity.Rare, TargetType.AnyEnemy, false)
+    {
+    }
+
+    protected override async Task OnPlay(
+        PlayerChoiceContext choiceContext,
+        CardPlay cardPlay)
+    {
+        ArgumentNullException.ThrowIfNull(cardPlay.Target);
+
+        await DamageCmd.Attack(DynamicVars.Damage.BaseValue)
+            .FromCard(this)
+            .Targeting(cardPlay.Target)
+            .WithHitFx("vfx/vfx_attack_slash")
+            .Execute(choiceContext);
+
+        await PowerCmd.Apply<FenShaoPower>(
+            choiceContext,
+            cardPlay.Target,
+            DynamicVars["FenShaoPower"].BaseValue,
+            Owner.Creature,
+            this);
+    }
+}
