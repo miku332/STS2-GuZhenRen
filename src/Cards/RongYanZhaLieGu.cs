a1a724390b8dfaf7b2f89d8fe1b8c0319c534a98
miku332 <112794032+miku332@users.noreply.github.com>
2026-08-09 00:34:02 +0800

    Add RongYanZhaLieGu

---
+++ src/Cards/RongYanZhaLieGu.cs
@@ -0,0 +1,61 @@
+using MegaCrit.Sts2.Core.Commands;
+using MegaCrit.Sts2.Core.Entities.Cards;
+using MegaCrit.Sts2.Core.GameActions.Multiplayer;
+using MegaCrit.Sts2.Core.Localization.DynamicVars;
+using MegaCrit.Sts2.Core.ValueProps;
+using STS2RitsuLib.Cards.DynamicVars;
+using STS2RitsuLib.Interop.AutoRegistration;
+using STS2RitsuLib.Scaffolding.Content;
+using GuZhenRen.CardPools;
+using GuZhenRen.Powers;
+using GuZhenRen.Tags;
+
+namespace GuZhenRen.Cards;
+
+[RegisterCard(typeof(GuZhenRenCardPool))]
+public sealed class RongYanZhaLieGu : GuZhenRenCardTemplate
+{
+    public override int Rank => IsUpgraded ? 4 : 3;
+
+    public override CardAssetProfile AssetProfile => new(
+        PortraitPath: "res://GuZhenRen/images/cards/RongYanZhaLieGu.png");
+
+    public override IEnumerable<CardTag> Tags => [GuZhenRenTags.YanDao];
+
+    protected override IEnumerable<DynamicVar> CanonicalVars =>
+    [
+        new DamageVar(7, ValueProp.Move),
+        new PowerVar<FenShaoPower>(7)
+    ];
+
+    public RongYanZhaLieGu()
+        : base(2, CardType.Attack, CardRarity.Common, TargetType.AnyEnemy, true)
+    {
+    }
+
+    protected override async Task OnPlay(
+        PlayerChoiceContext choiceContext,
+        CardPlay cardPlay)
+    {
+        ArgumentNullException.ThrowIfNull(cardPlay.Target);
+
+        await DamageCmd.Attack(DynamicVars.Damage.BaseValue)
+            .FromCard(this)
+            .Targeting(cardPlay.Target)
+            .WithHitFx("vfx/vfx_attack_slash")
+            .Execute(choiceContext);
+
+        await PowerCmd.Apply<FenShaoPower>(
+            choiceContext,
+            cardPlay.Target,
+            DynamicVars["FenShaoPower"].BaseValue,
+            Owner.Creature,
+            this);
+    }
+
+    protected override void OnUpgrade()
+    {
+        DynamicVars.Damage.UpgradeValueBy(2);
+        DynamicVars["FenShaoPower"].UpgradeValueBy(2);
+    }
+}
