87f92a7f9c853bc930a910bf47b4fd19b042e5f3
miku332 <112794032+miku332@users.noreply.github.com>
2026-08-09 02:58:44 +0800

    Add TianYuanBaoLian

---
+++ src/Cards/TianYuanBaoLian.cs
@@ -0,0 +1,45 @@
+using MegaCrit.Sts2.Core.Commands;
+using MegaCrit.Sts2.Core.Entities.Cards;
+using MegaCrit.Sts2.Core.GameActions.Multiplayer;
+using MegaCrit.Sts2.Core.Localization.DynamicVars;
+using STS2RitsuLib.Interop.AutoRegistration;
+using STS2RitsuLib.Scaffolding.Content;
+using GuZhenRen.CardPools;
+
+namespace GuZhenRen.Cards;
+
+[RegisterCard(typeof(GuZhenRenCardPool))]
+public sealed class TianYuanBaoLian : GuZhenRenCardTemplate
+{
+    public override int Rank => IsUpgraded ? 4 : 3;
+
+    public override CardAssetProfile AssetProfile => new(
+        PortraitPath: "res://GuZhenRen/images/cards/TianYuanBaoLian.png");
+
+    protected override IEnumerable<DynamicVar> CanonicalVars =>
+    [
+        new DynamicVar("Energy", 2)
+    ];
+
+    public TianYuanBaoLian()
+        : base(0, CardType.Skill, CardRarity.Common, TargetType.Self, true)
+    {
+    }
+
+    protected override async Task OnPlay(
+        PlayerChoiceContext choiceContext,
+        CardPlay cardPlay)
+    {
+        var energy = (int)DynamicVars["Energy"].BaseValue;
+        if (energy > 0)
+        {
+            await PlayerCmd.GainEnergy(energy, Owner);
+            DynamicVars["Energy"].BaseValue = Math.Max(0, energy - 1);
+        }
+    }
+
+    protected override void OnUpgrade()
+    {
+        DynamicVars["Energy"].UpgradeValueBy(1);
+    }
+}
