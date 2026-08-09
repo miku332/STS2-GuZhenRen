855bbe326c83c7e1550a8b19314760d6cd65da95
miku332 <112794032+miku332@users.noreply.github.com>
2026-08-09 13:45:49 +0800

    Add LeiGuDunGu

---
+++ src/Cards/LeiGuDunGu.cs
@@ -0,0 +1,66 @@
+using GuZhenRen.CardPools;
+using GuZhenRen.Powers;
+using MegaCrit.Sts2.Core.Commands;
+using MegaCrit.Sts2.Core.Entities.Cards;
+using MegaCrit.Sts2.Core.GameActions.Multiplayer;
+using MegaCrit.Sts2.Core.Localization.DynamicVars;
+using MegaCrit.Sts2.Core.ValueProps;
+using STS2RitsuLib.Cards.DynamicVars;
+using STS2RitsuLib.Interop.AutoRegistration;
+using STS2RitsuLib.Scaffolding.Content;
+
+namespace GuZhenRen.Cards;
+
+[RegisterCard(typeof(GuZhenRenCardPool))]
+public sealed class LeiGuDunGu : GuZhenRenCardTemplate
+{
+    public override int Rank => IsUpgraded ? 3 : 2;
+
+    public override CardAssetProfile AssetProfile => new(
+        PortraitPath: "res://GuZhenRen/images/cards/LeiGuDunGu.png");
+
+    public override bool GainsBlock => true;
+
+    protected override IEnumerable<DynamicVar> CanonicalVars =>
+    [
+        new BlockVar(6, ValueProp.Move),
+        new DynamicVar("HpLoss", 1),
+        new PowerVar<GuCiPower>(1)
+    ];
+
+    public LeiGuDunGu()
+        : base(1, CardType.Skill, CardRarity.Common, TargetType.Self, true)
+    {
+    }
+
+    protected override async Task OnPlay(
+        PlayerChoiceContext choiceContext,
+        CardPlay cardPlay)
+    {
+        await CreatureCmd.Damage(
+            choiceContext,
+            Owner.Creature,
+            DynamicVars["HpLoss"].BaseValue,
+            ValueProp.Unblockable | ValueProp.Unpowered,
+            Owner.Creature,
+            this);
+
+        await CreatureCmd.GainBlock(
+            Owner.Creature,
+            DynamicVars.Block.BaseValue,
+            DynamicVars.Block.Props,
+            cardPlay);
+
+        await PowerCmd.Apply<GuCiPower>(
+            choiceContext,
+            Owner.Creature,
+            DynamicVars["GuCiPower"].BaseValue,
+            Owner.Creature,
+            this);
+    }
+
+    protected override void OnUpgrade()
+    {
+        DynamicVars.Block.UpgradeValueBy(3);
+    }
+}
---
+++ src/Powers/GuCiPower.cs
@@ -0,0 +1,71 @@
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using MegaCrit.Sts2.Core.Combat;
+using MegaCrit.Sts2.Core.Commands;
+using MegaCrit.Sts2.Core.Entities.Cards;
+using MegaCrit.Sts2.Core.Entities.Creatures;
+using MegaCrit.Sts2.Core.Entities.Powers;
+using MegaCrit.Sts2.Core.GameActions.Multiplayer;
+using MegaCrit.Sts2.Core.Models;
+using MegaCrit.Sts2.Core.ValueProps;
+using STS2RitsuLib.Interop.AutoRegistration;
+using STS2RitsuLib.Scaffolding.Content;
+
+namespace GuZhenRen.Powers;
+
+[RegisterPower]
+public sealed class GuCiPower : ModPowerTemplate
+{
+    public override PowerType Type => PowerType.Buff;
+
+    public override PowerStackType StackType => PowerStackType.Counter;
+
+    public override PowerAssetProfile AssetProfile => new(
+        IconPath: "res://GuZhenRen/images/powers/GuCiPower.png",
+        BigIconPath: "res://GuZhenRen/images/powers/GuCiPower_p.png");
+
+    public override async Task AfterDamageReceived(
+        PlayerChoiceContext choiceContext,
+        Creature target,
+        DamageResult result,
+        ValueProp props,
+        Creature? dealer,
+        CardModel? cardSource)
+    {
+        if (target != Owner
+            || dealer is null
+            || dealer == Owner
+            || dealer.Player is not null
+            || Amount <= 0
+            || !props.IsPoweredAttack())
+        {
+            return;
+        }
+
+        var blockedDamage = result.TotalDamage - result.UnblockedDamage;
+        if (blockedDamage <= 0)
+        {
+            return;
+        }
+
+        Flash();
+        await CreatureCmd.Damage(
+            choiceContext,
+            dealer,
+            blockedDamage,
+            ValueProp.Unpowered,
+            Owner,
+            null);
+    }
+
+    public override async Task AfterSideTurnEnd(
+        PlayerChoiceContext choiceContext,
+        CombatSide side,
+        IEnumerable<Creature> participants)
+    {
+        if (side == CombatSide.Enemy)
+        {
+            await PowerCmd.Remove(this);
+        }
+    }
+}
