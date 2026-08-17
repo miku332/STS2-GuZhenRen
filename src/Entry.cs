70b74a83840780d4e07167d46dc144421b379cd3
miku332 <112794032+miku332@users.noreply.github.com>
2026-08-18 03:41:00 +0800

    chore: bump version to 0.4.11

--- src/Entry.cs
+++ src/Entry.cs
@@ -21,7 +21,7 @@ namespace GuZhenRen;
 public static class Entry
 {
     public const string ModId = "GuZhenRen";
-    public const string Version = "0.4.10";
+    public const string Version = "0.4.11";
 
     public static readonly Logger Logger = RitsuLibFramework.CreateLogger(ModId);
 
