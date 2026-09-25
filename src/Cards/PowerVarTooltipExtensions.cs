e3042ae16a4c8f3a5c9c525c38c119e7e7dc7c58
miku332 <112794032+miku332@users.noreply.github.com>
2026-09-25 22:36:28 +0800

    Match Constrict preview and color X values

--- src/Cards/PowerVarTooltipExtensions.cs
+++ src/Cards/PowerVarTooltipExtensions.cs
@@ -1,7 +1,9 @@
 using MegaCrit.Sts2.Core.HoverTips;
 using MegaCrit.Sts2.Core.Helpers;
+using MegaCrit.Sts2.Core.Localization;
 using MegaCrit.Sts2.Core.Localization.DynamicVars;
 using MegaCrit.Sts2.Core.Models;
+using MegaCrit.Sts2.Core.Models.Powers;
 using STS2RitsuLib.Cards.DynamicVars;
 
 namespace GuZhenRen.Cards;
@@ -19,8 +21,20 @@ internal static class PowerVarTooltipExtensions
         where T : PowerModel
     {
         var power = ModelDb.Power<T>();
+
+        if (typeof(T) == typeof(ConstrictPower))
+        {
+            var constrictDescription = new LocString(
+                "powers",
+                "GU_ZHEN_REN_POWER_CONSTRICT_PREVIEW.description");
+            return new HoverTip(
+                power,
+                constrictDescription.GetFormattedText(),
+                isSmart: false);
+        }
+
         var description = power.Description;
-        description.Add("Amount", "X");
+        description.Add("Amount", "[blue]X[/blue]");
         description.Add("singleStarIcon", "[img]res://images/packed/sprite_fonts/star_icon.png[/img]");
         description.Add("energyPrefix", EnergyIconHelper.GetPrefix(power));
         return new HoverTip(power, description.GetFormattedText(), isSmart: false);
